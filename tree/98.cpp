65cc907ae3da131087ab25607c5cd4d1de468442
Castle-3 <1301076746@qq.com>
2021-09-04 19:29:31 +0800

    add orther two methods in 98.cpp

--- tree/98.cpp
+++ tree/98.cpp
@@ -1,15 +1,59 @@
 #include"tree.h"
 
+// class Solution {
+// public:
+//     bool isValidBST(TreeNode* root) {
+//         return recursion(root, LONG_MIN, LONG_MAX);
+//     }
+
+//     bool recursion(TreeNode* root, long long low, long long high) {
+//         if(root == nullptr) return true;
+//         if(root->val <= low || root->val >= high) return false;
+//         return recursion(root->left, low, root->val) && recursion(root->right, root->val, high);
+//     }
+// };
+
+
+// // 递归法， 思路是中序遍历是否有序
+// class Solution {
+// public:
+//     vector<int> vec;
+
+//     bool isValidBST(TreeNode* root) {
+//         traversal(root);
+//         int size = vec.size();
+//         for(int i = 0; i < size - 1; i++){
+//             if(vec[i] >= vec[i + 1]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     void traversal(TreeNode* root){
+//         if(root == NULL) return;
+//         traversal(root->left);
+//         vec.push_back(root->val);
+//         traversal(root->right);
+//     }
+// };
+
+
+// 递归
 class Solution {
 public:
+    TreeNode* prev = NULL;
     bool isValidBST(TreeNode* root) {
-        return recursion(root, LONG_MIN, LONG_MAX);
-    }
+        if(root == NULL) return true;
+        
+        bool left = isValidBST(root->left);
 
-    bool recursion(TreeNode* root, long long low, long long high) {
-        if(root == nullptr) return true;
-        if(root->val <= low || root->val >= high) return false;
-        return recursion(root->left, low, root->val) && recursion(root->right, root->val, high);
+        if(prev != NULL && root->val <= prev->val) return false;
+        prev = root;
+
+        bool right = isValidBST(root->right);
+
+        return left && right;
     }
 };
 
